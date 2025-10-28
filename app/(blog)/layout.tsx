93ccf4ea9735391052f498d665b36db2a004a999
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-10-27 19:24:07 -0500

    fix: update aboutme link to aboutus in blog layout

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -124,7 +124,7 @@ export default async function RootLayout({
                     </h3>
                     <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
                       <Link
-                        href="/aboutme"
+                        href="/aboutus"
                         className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
                       >
                         Sobre nosotros
